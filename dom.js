834793e50b1a10128a27f0c2e27c6db6a8999f49
01919108033 <nadimshourov8@gmail.com>
2024-02-14 11:35:27 +0600

    some modify

--- dom.js
+++ dom.js
@@ -1,19 +1,41 @@
-const allSection = document.querySelectorAll("section")
-for (const section of allSection){
-    section.style.backgroundColor = "red";
-    section.style.padding = "15px"
-    section.style.marginBottom = "10px"
-    section.style.color = "white"
-}
-// section.style.backgroundColor = "red";
+// const allSection = document.querySelectorAll("section")
+// for (const section of allSection){
+//     section.style.backgroundColor = "red";
+//     section.style.padding = "15px"
+//     section.style.marginBottom = "10px"
+//     section.style.color = "white"
+// }
+// // section.style.backgroundColor = "red";
 
-const pAll = document.getElementById("part-2")
-const p = document.createElement("p")
-p.innerText = "i love bangladesh"
-pAll.appendChild(p);
-const h2All = document.getElementById("part-2")
-const h2 = document.createElement("h2")
-h2.innerText = "i love bangladesh"
-pAll.appendChild(h2);
+// const pAll = document.getElementById("part-2")
+// const p = document.createElement("p")
+// p.innerText = "i love bangladesh"
+// pAll.appendChild(p);
+// const h2All = document.getElementById("part-2")
+// const h2 = document.createElement("h2")
+// h2.innerText = "i love bangladesh"
+// pAll.appendChild(h2);
 
-const h3All
+// function makegreen() {
+//     document.body.style.backgroundColor='green'
+//    }
+//    const aquaColor = document.getElementById("aqua")
+//    aquaColor.onclick = makeAqua;
+//    function makeAqua() {
+//     document.body.style.backgroundColor="aqua"
+//    }
+//    document.getElementById("final").addEventListener('click',function(){
+//     document.body.style.backgroundColor="tomato"
+//    })
+//    document.getElementById("retake").addEventListener('click' , function () {
+//     const changeText =document.getElementById("nadim")
+//     changeText.innerText = "this is shourov"
+//    })
+//    document.getElementById("update").addEventListener('click', function(){
+//     const inputFeild = document.getElementById("input")
+//  // const inputFeild = document.getElementById()
+//     const inputText = inputFeild.value;
+//     const p = document.getElementById("name");
+//     p.innerText = inputText;
+//     inputFeild.value = ""
+//    })
